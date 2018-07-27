ab390f4526dff90aca43b296ac54e9a0df2928ae
gdyuki <gdygdyukimy@outlook.jp>
2018-07-27 20:46:52 +0900

    Add files via upload

---
+++ common.py
@@ -0,0 +1,6 @@
+print("hello,world")
+int_s = 0
+for AddNum in range(1,51):
+    int_s += x
+
+print(int_s)
---
+++ moimoiGame.py
@@ -0,0 +1,21 @@
+import random
+
+while True:
+    while True:
+        com = random.randint(1,3)
+        com_ai = {1:"ぐー", 2:"チョキ", 3:"パー"}
+        player = input("じゃんけんしましょう。　ぐーは１　チョキは２　パーは３を入れてね")
+        if player == "1" or player == "2" or player == "3":
+            player = int(player)
+            break
+        else :
+            print("有効な数字を入れてください")
+
+
+
+    if player - com == -1 or player - com == 2 :
+        print("相手："+com_ai[com]+"  あなた："+com_ai[player]+"  あなたのかち！！")
+    elif player - com == 0:
+        print("相手："+com_ai[com]+"  あなた："+com_ai[player]+"  アイコですたね")
+    else :
+        print("相手："+com_ai[com]+"  あなた："+com_ai[player]+"  あなたの負け")
